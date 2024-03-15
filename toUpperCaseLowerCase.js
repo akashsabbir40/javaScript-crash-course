67e8885e75b7e8e5961bada3cbd3111fcb6f7d52
Sabbluck19 <akashsabbir40@gmail.com>
2024-03-15 21:34:33 +0600

    use uppercase lowercase

---
+++ toUpperCaseLowerCase.js
@@ -0,0 +1,6 @@
+var love = "lorem ipsum is a dummy text used to replace text in some areas just for the purpose of an example. It can be used in publishing and graphic design."
+// console.log(love.toUpperCase());
+
+var Love = "LOREM IPSUM IS A DUMMY TEXT USED TO REPLACE TEXT IN SOME AREAS JUST fOR THE PURPOSE OF AN EXAMPLE. IT CAN BE USED IN PUBLISHING AND GRAPHIC DESIGN."
+
+console.log(love.toLowerCase());
