d4107211101fa72b5515a60184c08e462b8aa460
Sabbluck19 <akashsabbir40@gmail.com>
2024-03-15 22:18:45 +0600

    nothing use

--- parseInFloat.js
+++ parseInFloat.js
@@ -1,7 +1,13 @@
-var num1 = '55.7979779';
-var num2 = 89;
-// num1 = parseFloat(num1);
-num1 = parseInt(num1);
-var total = num1 + num2;
-console.log(total);
-// console.log(typeof num1);
+// var num1 = '55.7979779';
+// var num2 = 89;
+// // num1 = parseFloat(num1);
+// num1 = parseInt(num1);
+// var total = num1 + num2;
+// console.log(total);
+// // console.log(typeof num1);
+
+var num3 = 60;
+var num4 = 80;
+// num3 = "" + num3;
+
+console.log(typeof num3)
